929a842133e0dbc0ea11be06d5b0bbfbed34be99
Karima <elnadykarima@gmail.com>
2025-12-16 18:32:59 +0200

    theme color schemes

---
+++ lib/core/theme/color_schemes.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+const darkColorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: Color(0xFF00E5C1),
+  onPrimary: Colors.black,
+  secondary: Color(0xFF4DD0E1),
+  onSecondary: Colors.black,
+  background: Color(0xFF0E1F1C),
+  onBackground: Colors.white,
+  surface: Color(0xFF132824),
+  onSurface: Colors.white,
+  outline: Color(0xFF2E4F4A),
+  error: Color(0xFFFF6B6B),
+  onError: Colors.black,
+);
+
+const lightColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xFF00BFA5),
+  onPrimary: Colors.white,
+  secondary: Color(0xFF26A69A),
+  onSecondary: Colors.white,
+  background: Color(0xFFF6FBFA),
+  onBackground: Color(0xFF10201E),
+  surface: Colors.white,
+  onSurface: Color(0xFF10201E),
+  outline: Color(0xFFB2D8D2),
+  error: Color(0xFFD32F2F),
+  onError: Colors.white,
+);
